4b21e1b214e0c45006cec2f9f33c8e211b9ce8c9
JosedaMachine <jrave@ucm.es>
2022-04-22 12:02:13 +0200

    Ejercicio 2.2.1 y 2.2.2 Completados

--- practica2.2/1_serialization/ejercicio1.cc
+++ practica2.2/1_serialization/ejercicio1.cc
@@ -9,6 +9,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#define NAME_SIZE 80
+#define SIZE_RD_BUF 256
+
 class Jugador: public Serializable
 {
 public:
@@ -19,20 +22,33 @@ public:
 
     virtual ~Jugador(){};
 
-    void to_bin()
-    {
-        //
+    void to_bin(){
+                        //size of x , y and name
+        int32_t size = 2 *sizeof(int16_t) + NAME_SIZE* sizeof(char);
+
+        alloc_data(size);
+
+        char* tmp = _data;
+
+        memcpy(tmp, name, NAME_SIZE * sizeof(char));
+
+        tmp+= NAME_SIZE * sizeof(char);
+
+        memcpy(tmp, &x, sizeof(int16_t));
+
+        tmp+= sizeof(int16_t);
+
+        memcpy(tmp, &y, sizeof(int16_t));
     }
 
-    int from_bin(char * data)
-    {
-        //
+    int from_bin(char * data) {
+        
         return 0;
     }
 
 
 public:
-    char name[80];
+    char name[NAME_SIZE];
 
     int16_t x;
     int16_t y;
@@ -43,10 +59,29 @@ int main(int argc, char **argv)
     Jugador one_r("", 0, 0);
     Jugador one_w("Player_ONE", 123, 987);
 
+    const char* name = "./jugador.bin";
+
     // 1. Serializar el objeto one_w
+    one_w.to_bin();
+
     // 2. Escribir la serialización en un fichero
+    int fd = open(name, O_CREAT|O_TRUNC | O_RDWR, 0666);
+    write(fd, one_w.data(), one_w.size());
+
+    close(fd);
     // 3. Leer el fichero
+    // char buff[SIZE_RD_BUF];
+
+    // fd = open(name, O_RDONLY);
+
+    // read(fd, buff, SIZE_RD_BUF);
+
+    // close(fd);
+    // one_r.from_bin(buff);
+    
     // 4. "Deserializar" en one_r
+
+    
     // 5. Mostrar el contenido de one_r
 
     return 0;
--- practica2.2/1_serialization/ex1.cc
+++ practica2.2/1_serialization/ex1.cc
@@ -6,10 +6,9 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/netdb.h>
-
+#include <netdb.h>
 
 int main(int argc, char *argv[]){
-    
+        
     return 0;
 }
